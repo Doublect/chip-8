34788bbf9c5a781c38586eb730deab98d9988a7f
Hunor Liszka <hunor.liszka@protonmail.com>
2022-11-09 21:25:48 +0000

    Flesh out instructions Add font loading

--- src/interpreter/mod.rs
+++ src/interpreter/mod.rs
@@ -11,10 +11,15 @@ pub struct Interpreter {
     PC: u16,
     delay_timer: u8,
     sound_timer: u8,
+
+    delay_count: u8,
+    sound_count: u8,
+
     SP: u8,
     stack: [u16; 16],
     clock_speed: u64,
     keys: [bool; 16],
+    waiting_for_key: Option<u8>,
 
     Display: Display,
 }
@@ -26,12 +31,16 @@ impl Interpreter {
             V: [0; 16],
             nibble_holder: (0, 0, 0, 0),
             I: 0,
-            PC: 0, //0x200
+            PC: 0x200,
             delay_timer: 0,
             sound_timer: 0,
+            delay_count: 0,
+            sound_count: 0,
             SP: 0,
             stack: [0; 16],
             clock_speed: 700,
+            keys: [false; 16],
+            waiting_for_key: None,
             Display: [[Pixel::Black; 64]; 32],
         }
     }
@@ -56,6 +65,7 @@ impl Interpreter {
     }
 
     fn execute(&mut self, opcode: (u8, u8, u8, u8)) {
+        println!("EXECUTE {:?}, PC = {:?}", opcode, self.PC);
         match opcode {
             (0x0, 0x0, 0xE, 0x0) => {
                 self.Display = [[Pixel::Black; 64]; 32];
@@ -94,7 +104,7 @@ impl Interpreter {
                 self.V[x as usize] = Interpreter::get_byte(y, z);
             },
             (0x7, x, y, z) => {
-                self.V[x as usize] += Interpreter::get_byte(y, z);
+                self.V[x as usize] = self.V[x as usize].wrapping_add(Interpreter::get_byte(y, z));
             },
             (0x8, x, y, 0x0) => {
                 self.V[x as usize] = self.V[y as usize];
@@ -109,7 +119,7 @@ impl Interpreter {
                 self.V[x as usize] ^= self.V[y as usize];
             },
             (0x8, x, y, 0x4) => {
-                let res = self.V[x as usize] as u16 + self.V[y as usize] as u16;
+                let mut res = self.V[x as usize] as u16 + self.V[y as usize] as u16;
 
                 if(res > 264) {
                     self.V[0xF] = 1;
@@ -158,43 +168,51 @@ impl Interpreter {
             (0xC, x, y, z) => {
                 self.V[x as usize] = Interpreter::get_byte(y, z) & rand::random::<u8>();
             },
-            (0xD, x, y, z) => {
+            (0xD, x, y, n) => {
                 let x = self.V[x as usize] as usize % 64;
                 let y = self.V[y as usize] as usize % 32;
                 self.V[0xF] = 0;
 
-                let height = (z as f64).div(8.0).ceil() as usize;
                 let mut memory_line: u8;
-
                 self.V[0xF] = 0;
 
-                for yline in 0..height {
+                for yline in 0..n as usize {
+                    if y + yline >= 32 {
+                        break;
+                    }
                     memory_line = self.memory[self.I as usize + yline];
                     for xline in 0..8 {
-                        if (memory_line & (0x80 >> xline)) != 0 {
-                            if self.Display[y + yline][x + xline] == Pixel::White {
-                                self.V[0xF] = 1;
-                            }
-                            self.Display[y + yline][x + xline] ^= Pixel::White;
+                        if x + xline >= 64 {
+                            break;
                         }
+
+                        if self.Display[y + yline][x + xline] == Pixel::White {
+                            self.V[0xF] = 1;
+                        }
+                        self.Display[y + yline][x + xline] ^= 
+                            if memory_line & (0x80 >> xline) == 0 { Pixel::Black } else { Pixel::White };
                     }
                 }
             },
             (0xE, x, 0x9, 0xE) => {
-                if self.keys[self.memory[x as usize] as usize] {
-                    self.PC += 2;
-                }
+                self.keys.get(self.memory[x as usize] as usize)
+                    .and_then(|res| {
+                    if *res { self.PC += 2 }; 
+                    Some(res) 
+                });
             },
             (0xE, x, 0xA, 0x1) => {
-                if !self.keys[self.memory[x as usize] as usize] {
-                    self.PC += 2;
-                }
+                self.keys.get(self.memory[x as usize] as usize)
+                    .and_then(|res| {
+                    if !*res { self.PC += 2 }; 
+                    Some(res) 
+                });
             },
             (0xF, x, 0x0, 0x7) => {
                 self.V[x as usize] = self.delay_timer;
             },
             (0xF, x, 0x0, 0xA) => {
-                todo!()
+                self.waiting_for_key = Some(x);
             },
             (0xF, x, 0x1, 0x5) => {
                 self.delay_timer = self.memory[x as usize];
@@ -209,21 +227,22 @@ impl Interpreter {
                 self.I = self.memory[x as usize] as u16 * 5;
             },
             (0xF, x, 0x3, 0x3) => {
-                todo!()
                 self.memory[self.I as usize] = self.memory[x as usize] / 100;
                 self.memory[self.I as usize + 1] = (self.memory[x as usize] / 10) % 10;
                 self.memory[self.I as usize + 2] = (self.memory[x as usize] % 100) % 10;
             },
             (0xF, x, 0x5, 0x5) => {
                 for i in 0..=x {
-                    self.memory[self.I as usize + i] = self.memory[i as usize];
+                    self.memory[self.I as usize + i as usize] = self.memory[i as usize];
                 }
             },
             (0xF, x, 0x6, 0x5) => {
                 for i in 0..=x {
-                    self.memory[i as usize] = self.memory[self.I as usize + i];
+                    self.memory[i as usize] = self.memory[self.I as usize + i as usize];
                 }
             },
+            _ => {
+                println!("Unknown opcode: {:?}", opcode);
             }
         }
     }
@@ -235,12 +254,81 @@ impl Interpreter {
     fn get_byte(x: u8, y: u8) -> u8 {
         ((x as u8) << 4) | (y as u8)
     }
+
+    pub fn load(&mut self, data: Vec<u8>) {
+        self.load_fonts();
+        data.iter().enumerate().for_each(|(i, byte)| {
+            // println!("{}: {}", i, byte);
+            self.memory[0x200 + i] = *byte;
+        })
+    }
+
+    pub fn load_fonts(&mut self) {
+        let font_data = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+        ];
+
+        font_data.iter().enumerate().for_each(|(i, byte)| {
+            self.memory[i] = *byte;
+        })
+    }
 }
 
 
 impl chip8_base::Interpreter for Interpreter {
     fn step(&mut self, keys: &Keys) -> Option<Display> {
-        None
+        self.keys = keys.clone();
+
+        // Timers
+        if self.delay_timer > 0 {
+            self.delay_count += 1;
+
+            if self.delay_count == 60 {
+                self.delay_timer -= 1;
+                self.delay_count = 0;
+            }
+        }
+
+        if self.sound_timer > 0 {
+            self.sound_count += 1;
+
+            if self.sound_count == 60 {
+                self.sound_timer -= 1;
+                self.sound_count = 0;
+            }
+        }
+
+        if(self.waiting_for_key.is_some()) {
+            for (i, key) in self.keys.iter().enumerate() {
+                if *key {
+                    self.memory[self.V[self.waiting_for_key.unwrap() as usize] as usize] = i as u8;
+                    self.waiting_for_key = None;
+
+                    break;
+                }
+            }
+        } else {
+            let opcode = self.fetch();
+
+            self.execute(opcode);
+        }
+
+        Some(self.Display)
     }
 
     fn speed(&self) -> std::time::Duration {
@@ -248,6 +336,10 @@ impl chip8_base::Interpreter for Interpreter {
     }
 
     fn buzzer_active(&self) -> bool {
-        false
+        if self.sound_timer > 0 {
+            true
+        } else {
+            false
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,12 @@ mod interpreter;
 fn main() {
     let mut interpreter  = Interpreter::new();
 
+    let rom = std::fs::read("./roms/IBM Logo.ch8").unwrap();
+    println!("READ ROM");
+    println!("ROM SIZE: {}", rom.len());
+    interpreter.load(rom);
+    println!("LOADED ROM");
+
+    println!("STARTING CHIP-8");
     chip8_base::run(interpreter)
 }
